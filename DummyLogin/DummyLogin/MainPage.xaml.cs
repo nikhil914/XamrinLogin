ea10516cb5b237a40f4868c78966aba56491d742
Nikhil Ranjan <nikhilranjan914@gmail.com>
2021-05-24 18:14:55 +0530

    Add project files.

---
+++ DummyLogin/DummyLogin/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DummyLogin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  void Button_ClickedAsync(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(email.Text);
+            bool isPassowrdEmpty = string.IsNullOrEmpty(password.Text);
+
+            if (isEmailEmpty || isPassowrdEmpty)
+            {
+                // Empty  so warning
+                Console.Write("Empty");
+            }
+            else
+            {
+                Console.Write("Not Empty");
+
+
+                // Go aheed Check id and password 
+                 Navigation.PushAsync(new Page2());
+            }
+        }
+
+        private void signup_Cliced(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+
+        }
+    }
+}
---
+++ DummyLogin/DummyLogin/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DummyLogin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
